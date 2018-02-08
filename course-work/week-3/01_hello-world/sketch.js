01e1b2ae57c383cc2fe7a24e34c0352294591f43
Kur19 <kurtis.hassinger@gmail.com>
2018-02-07 22:52:49 -0700

    Adding coursework p5 examples and committing hw-3

---
+++ course-work/week-3/01_hello-world/sketch.js
@@ -0,0 +1,3 @@
+function setup(){
+  text("Hello Kurtis!", 0, 10);
+}
---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+// Declare the setup function
+function setup() {
+
+  // create a canvas 700px wide and 500px high
+  createCanvas( 700, 500 );
+
+  // color the background 'green'
+  background( 'green' );
+}
+
+
+// Declare draw() function
+function draw(){
+
+  // using the text function, have p5 write, "Hello World!"
+  text( "Hello World!", 250, 150 );
+}
